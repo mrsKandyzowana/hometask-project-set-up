9de6125ca6cc5b6e9b67278028020e70aab7530d
Paulina Kmiecińska <paulinakmiecinska@gmail.com>
2022-11-09 23:02:47 +0100

    homework done

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # hometask-project-set-up
+
+#### Step 1: check the project sctructure
+
+Check the project structure and the set up:
+
+- package.json file with dependencies
+- js folder with files
+- .gitignore file
+- node_modules file
+- tests file
+- babel confirguration file
+
+#### Step 2: build the project locally
+
+Once the path is correct, run the `yarn` command in order to set up all project dependencies\*\*
+After the installation, the project has to be started to start the local testing
+
+#### Step 3: check the jest installation and run tests
+
+Check your "./package.json" file. As a dependencie you're supposed to see Cucumber:
+
+```json
+  "devDependencies": {
+    "jest": "^25.5.4",
+  }
+```
+
+#### Step 4: check tests
+
+Run 'yarn test' to check the result.
---
+++ tests/rectangle.test.js
@@ -0,0 +1,21 @@
+import { getRectanglePerimeter, getRectangleArea, getRectangleInfo } from '../js/rectangle.js';
+
+// var rectangle = require('../js/rectangle');
+// var getRectanglePerimeter = require('getRectanglePerimeter');
+
+test('should properly output the perimeter of rectangle', ()=>{
+    const outputPerimeter = getRectanglePerimeter(20, 34);
+    expect(outputPerimeter).toBe(108);
+});
+
+test('should properly output the area of rectangle', ()=>{
+    const outputArea = getRectangleArea(70, 94);
+    expect(outputArea).toBe(6580);
+});
+
+test('should properly output the info on rectangle', ()=>{
+    const outputRectangleInfo = getRectangleInfo((74,26), (74,26));
+    const outarea = getRectangleArea(74, 26);
+    const outperimeter = getRectanglePerimeter(74, 26);
+    expect(outputRectangleInfo).toBe(`The perimeter of a rectangle is ${outperimeter} and the area is ${outarea}`);
+});
